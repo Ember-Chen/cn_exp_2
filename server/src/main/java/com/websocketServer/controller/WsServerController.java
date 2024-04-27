9ea2ff4a111e38e709c7d5bb9cfc90b00110b8d5
Vu1c4n <1633080461@qq.com>
2024-04-27 12:14:45 +0800

    add: client

---
+++ server/src/main/java/com/websocketServer/controller/WsServerController.java
@@ -0,0 +1,22 @@
+package com.websocketServer.controller;
+
+import com.websocketServer.server.WsServer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WsServerController {
+    @Autowired
+    WsServer wsServer;
+
+    /**
+     * 服务端发消息给客户端
+     * @param message 消息
+     */
+    @PostMapping("/send2client")
+    public void send2Client(@RequestBody String message){
+        WsServer.broadcast(message,"txt");
+    }
+}
